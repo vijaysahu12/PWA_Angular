f40a3ffefbb54e1f0b5aa4440c4d86d826a08a11
Vijay Sahu <vijay.sahu12@gmail.com>
2020-02-06 12:26:06 +0530

    Added X cordinates

--- src/app/charts/custom-chart/custom-chart.component.ts
+++ src/app/charts/custom-chart/custom-chart.component.ts
@@ -16,12 +16,36 @@ export class CustomChartComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    this.GetVerticalBarCharData();
     this.GetChartValues();
   }
 
 
   GetChartValues() {
+    let calculatedWidth = ((100 - this.ChartData.length + 1) / this.ChartData.length);
+    if (calculatedWidth > 6) {
+      calculatedWidth = 6;
+    } else {
+      calculatedWidth = parseInt(calculatedWidth);
+    }
 
+    for (let i = 0; this.ChartData.length > i; i++) {
+      this.ChartData[i].Width = calculatedWidth.toString() + '%';
+      if ( i === 0 ) {
+        this.ChartData[i].MarginLeft = '1%';
+      } else {
+        this.ChartData[i].MarginLeft =
+        (calculatedWidth + parseInt(this.ChartData[i - 1].MarginLeft.split('%')[0])  + 1).toString() + '%';
+
+        this.ChartData[i].MarginLeft = ((i + 1) + (i * calculatedWidth)) + '%';
+      }
+      console.log( 'Margin Left: ' + this.ChartData[i].MarginLeft);
+    }
+  }
+
+
+
+  GetVerticalBarCharData() {
     this.ChartData = [{
       BarTopValue: 20 + '%',
       Height: 40 + '%',
@@ -101,22 +125,297 @@ export class CustomChartComponent implements OnInit {
       Width: 2 + '%',
       bgColor: 'rgb(179, 179, 179)',
       EmployeeName: 'Shivangi Rao'
-    }
-  ];
-    let calculatedWidth = (100 / this.ChartData.length);
-    if (calculatedWidth > 6) {
-      calculatedWidth = 6;
-    }
+    },
+    {
+      BarTopValue: 60 + '%',
+      Height: 60 + '%',
+      MarginLeft: 6 + '%',
+      Width: 2 + '%',
+      bgColor: 'pink',
+      EmployeeName: 'Annkur Shandilya'
+    },
+    {
+      BarTopValue: 90 + '%',
+      Height: 90 + '%',
+      MarginLeft: 10 + '%',
+      Width: 2 + '%',
+      bgColor: '#95d595',
+      EmployeeName: 'Swati Chintapatla'
+    },
+    {
+      BarTopValue: 10 + '%',
+      Height: 10 + '%',
+      MarginLeft: 14 + '%',
+      Width: 2 + '%',
+      bgColor: 'rgb(179, 179, 179)',
+      EmployeeName: 'Aman Pamecha'
+    },
+    {
+      BarTopValue: 55 + '%',
+      Height: 55 + '%',
+      MarginLeft: 14 + '%',
+      Width: 2 + '%',
+      bgColor: 'rgb(194, 235, 226)',
+      EmployeeName: 'Garima Khare'
+    },
+    {
+      BarTopValue: 35 + '%',
+      Height: 35 + '%',
+      MarginLeft: 14 + '%',
+      Width: 2 + '%',
+      bgColor: 'rgb(249, 203, 220)',
+      EmployeeName: 'Amit Bhujwal'
+    },
+    {
+      BarTopValue: 72 + '%',
+      Height: 72 + '%',
+      MarginLeft: 14 + '%',
+      Width: 2 + '%',
+      bgColor: 'pink',
+      EmployeeName: 'Shivangi Rao'
+    } ,
+     {
+      BarTopValue: 100 + '%',
+      Height: 100 + '%',
+      MarginLeft: 14 + '%',
+      Width: 2 + '%',
+      bgColor: 'rgb(249, 203, 220)',
+      EmployeeName: 'Amit Bhujwal'
+    },
 
-    for (let i = 0; this.ChartData.length > i; i++) {
-      this.ChartData[i].Width = calculatedWidth.toString() + '%';
-      if ( i === 0 ) {
-        this.ChartData[i].MarginLeft = '1%';
-      } else {
-        this.ChartData[i].MarginLeft =
-        (calculatedWidth + parseInt(this.ChartData[i - 1].MarginLeft.split('%')[0])  + 1).toString() + '%';
-      }
-      console.log( 'Margin Left' + this.ChartData[i].MarginLeft);
-    }
+    // ,
+    // {
+    //   BarTopValue: 67 + '%',
+    //   Height: 67 + '%',
+    //   MarginLeft: 14 + '%',
+    //   Width: 2 + '%',
+    //   bgColor: '#ffaeae',
+    //   EmployeeName: 'Shivangi Rao'
+    // },
+    // {
+    //   BarTopValue: 89 + '%',
+    //   Height: 89 + '%',
+    //   MarginLeft: 14 + '%',
+    //   Width: 2 + '%',
+    //   bgColor: 'rgb(252, 238, 189)',
+    //   EmployeeName: 'Shivangi Rao'
+    // },
+    // {
+    //   BarTopValue: 62 + '%',
+    //   Height: 62 + '%',
+    //   MarginLeft: 14 + '%',
+    //   Width: 2 + '%',
+    //   bgColor: 'rgb(179, 179, 179)',
+    //   EmployeeName: 'Shivangi Rao'
+    // },
+    // {
+    //   BarTopValue: 55 + '%',
+    //   Height: 55 + '%',
+    //   MarginLeft: 14 + '%',
+    //   Width: 2 + '%',
+    //   bgColor: 'rgb(194, 235, 226)',
+    //   EmployeeName: 'Garima Khare'
+    // },
+    // {
+    //   BarTopValue: 35 + '%',
+    //   Height: 35 + '%',
+    //   MarginLeft: 14 + '%',
+    //   Width: 2 + '%',
+    //   bgColor: 'rgb(249, 203, 220)',
+    //   EmployeeName: 'Amit Bhujwal'
+    // },
+    // {
+    //   BarTopValue: 72 + '%',
+    //   Height: 72 + '%',
+    //   MarginLeft: 14 + '%',
+    //   Width: 2 + '%',
+    //   bgColor: 'pink',
+    //   EmployeeName: 'Shivangi Rao'
+    // },
+    // {
+    //   BarTopValue: 67 + '%',
+    //   Height: 67 + '%',
+    //   MarginLeft: 14 + '%',
+    //   Width: 2 + '%',
+    //   bgColor: '#ffaeae',
+    //   EmployeeName: 'Shivangi Rao'
+    // },
+    // {
+    //   BarTopValue: 89 + '%',
+    //   Height: 89 + '%',
+    //   MarginLeft: 14 + '%',
+    //   Width: 2 + '%',
+    //   bgColor: 'rgb(252, 238, 189)',
+    //   EmployeeName: 'Shivangi Rao'
+    // },
+    // {
+    //   BarTopValue: 62 + '%',
+    //   Height: 62 + '%',
+    //   MarginLeft: 14 + '%',
+    //   Width: 2 + '%',
+    //   bgColor: 'rgb(179, 179, 179)',
+    //   EmployeeName: 'Shivangi Rao'
+    // },
+    // {
+    //   BarTopValue: 72 + '%',
+    //   Height: 72 + '%',
+    //   MarginLeft: 14 + '%',
+    //   Width: 2 + '%',
+    //   bgColor: 'pink',
+    //   EmployeeName: 'Shivangi Rao'
+    // },
+    // {
+    //   BarTopValue: 67 + '%',
+    //   Height: 67 + '%',
+    //   MarginLeft: 14 + '%',
+    //   Width: 2 + '%',
+    //   bgColor: '#ffaeae',
+    //   EmployeeName: 'Shivangi Rao'
+    // },
+    // {
+    //   BarTopValue: 89 + '%',
+    //   Height: 89 + '%',
+    //   MarginLeft: 14 + '%',
+    //   Width: 2 + '%',
+    //   bgColor: 'rgb(252, 238, 189)',
+    //   EmployeeName: 'Shivangi Rao'
+    // },
+    // {
+    //   BarTopValue: 62 + '%',
+    //   Height: 62 + '%',
+    //   MarginLeft: 14 + '%',
+    //   Width: 2 + '%',
+    //   bgColor: 'rgb(179, 179, 179)',
+    //   EmployeeName: 'Shivangi Rao'
+    // },
+    // {
+    //   BarTopValue: 55 + '%',
+    //   Height: 55 + '%',
+    //   MarginLeft: 14 + '%',
+    //   Width: 2 + '%',
+    //   bgColor: 'rgb(194, 235, 226)',
+    //   EmployeeName: 'Garima Khare'
+    // },
+    // {
+    //   BarTopValue: 35 + '%',
+    //   Height: 35 + '%',
+    //   MarginLeft: 14 + '%',
+    //   Width: 2 + '%',
+    //   bgColor: 'rgb(249, 203, 220)',
+    //   EmployeeName: 'Amit Bhujwal'
+    // },
+    // {
+    //   BarTopValue: 72 + '%',
+    //   Height: 72 + '%',
+    //   MarginLeft: 14 + '%',
+    //   Width: 2 + '%',
+    //   bgColor: 'pink',
+    //   EmployeeName: 'Shivangi Rao'
+    // },
+    // {
+    //   BarTopValue: 67 + '%',
+    //   Height: 67 + '%',
+    //   MarginLeft: 14 + '%',
+    //   Width: 2 + '%',
+    //   bgColor: '#ffaeae',
+    //   EmployeeName: 'Shivangi Rao'
+    // },
+    // {
+    //   BarTopValue: 89 + '%',
+    //   Height: 89 + '%',
+    //   MarginLeft: 14 + '%',
+    //   Width: 2 + '%',
+    //   bgColor: 'rgb(252, 238, 189)',
+    //   EmployeeName: 'Shivangi Rao'
+    // },
+    // {
+    //   BarTopValue: 62 + '%',
+    //   Height: 62 + '%',
+    //   MarginLeft: 14 + '%',
+    //   Width: 2 + '%',
+    //   bgColor: 'rgb(179, 179, 179)',
+    //   EmployeeName: 'Shivangi Rao'
+    // },
+    // {
+    //   BarTopValue: 72 + '%',
+    //   Height: 72 + '%',
+    //   MarginLeft: 14 + '%',
+    //   Width: 2 + '%',
+    //   bgColor: 'pink',
+    //   EmployeeName: 'Shivangi Rao'
+    // },
+    // {
+    //   BarTopValue: 67 + '%',
+    //   Height: 67 + '%',
+    //   MarginLeft: 14 + '%',
+    //   Width: 2 + '%',
+    //   bgColor: '#ffaeae',
+    //   EmployeeName: 'Shivangi Rao'
+    // },
+    // {
+    //   BarTopValue: 89 + '%',
+    //   Height: 89 + '%',
+    //   MarginLeft: 14 + '%',
+    //   Width: 2 + '%',
+    //   bgColor: 'rgb(252, 238, 189)',
+    //   EmployeeName: 'Shivangi Rao'
+    // },
+    // {
+    //   BarTopValue: 62 + '%',
+    //   Height: 62 + '%',
+    //   MarginLeft: 14 + '%',
+    //   Width: 2 + '%',
+    //   bgColor: 'rgb(179, 179, 179)',
+    //   EmployeeName: 'Shivangi Rao'
+    // },
+    // {
+    //   BarTopValue: 55 + '%',
+    //   Height: 55 + '%',
+    //   MarginLeft: 14 + '%',
+    //   Width: 2 + '%',
+    //   bgColor: 'rgb(194, 235, 226)',
+    //   EmployeeName: 'Garima Khare'
+    // },
+    // {
+    //   BarTopValue: 35 + '%',
+    //   Height: 35 + '%',
+    //   MarginLeft: 14 + '%',
+    //   Width: 2 + '%',
+    //   bgColor: 'rgb(249, 203, 220)',
+    //   EmployeeName: 'Amit Bhujwal'
+    // },
+    // {
+    //   BarTopValue: 72 + '%',
+    //   Height: 72 + '%',
+    //   MarginLeft: 14 + '%',
+    //   Width: 2 + '%',
+    //   bgColor: 'pink',
+    //   EmployeeName: 'Shivangi Rao'
+    // },
+    // {
+    //   BarTopValue: 67 + '%',
+    //   Height: 67 + '%',
+    //   MarginLeft: 14 + '%',
+    //   Width: 2 + '%',
+    //   bgColor: '#ffaeae',
+    //   EmployeeName: 'Shivangi Rao'
+    // },
+    // {
+    //   BarTopValue: 89 + '%',
+    //   Height: 89 + '%',
+    //   MarginLeft: 14 + '%',
+    //   Width: 2 + '%',
+    //   bgColor: 'rgb(252, 238, 189)',
+    //   EmployeeName: 'Shivangi Rao'
+    // },
+    // {
+    //   BarTopValue: 62 + '%',
+    //   Height: 62 + '%',
+    //   MarginLeft: 14 + '%',
+    //   Width: 2 + '%',
+    //   bgColor: 'rgb(179, 179, 179)',
+    //   EmployeeName: 'Shivangi Rao'
+    // }
+  ];
   }
 }
